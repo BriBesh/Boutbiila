c975fa03678e42a4e88251c66f98665a39791f41
barbeech <78350838+BriBesh@users.noreply.github.com>
2022-01-31 23:19:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     clickedButton = this.innerHTML;
+    buttonAnimation(clickedButton);
     makeSound(clickedButton);
   });
 
